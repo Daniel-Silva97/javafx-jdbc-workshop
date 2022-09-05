70f75bc299a56ac4c91ccf1cc31dded5ccb1958e
Daniel da silva pedro <danielsp699@outlook.com>
2022-09-05 19:15:26 -0300

    Saving a new Department

--- src/main/java/com/projetojavafx/javafxjdbcworkshop/DepartmentFormController.java
+++ src/main/java/com/projetojavafx/javafxjdbcworkshop/DepartmentFormController.java
@@ -1,10 +1,15 @@
 package com.projetojavafx.javafxjdbcworkshop;
 
+import com.projetojavafx.javafxjdbcworkshop.db.DbException;
 import com.projetojavafx.javafxjdbcworkshop.gui.util.Alerts;
 import com.projetojavafx.javafxjdbcworkshop.gui.util.Constraints;
+import com.projetojavafx.javafxjdbcworkshop.gui.util.Utils;
 import com.projetojavafx.javafxjdbcworkshop.model.entities.Department;
+import com.projetojavafx.javafxjdbcworkshop.model.services.DepartmentService;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -15,6 +20,7 @@ import java.util.ResourceBundle;
 public class DepartmentFormController implements Initializable {
 
     private Department entity;
+    private DepartmentService service;
     @FXML
     private TextField txtId;
     @FXML
@@ -30,22 +36,39 @@ public class DepartmentFormController implements Initializable {
         this.entity = entity;
     }
 
-    public void updateFormData() {
+    public void setDepartmentService(DepartmentService service) {
+        this.service = service;
+    }
+
+
+    @FXML
+    public void onBtSaveAction(ActionEvent event) {
         if (entity == null) {
-            throw new IllegalStateException("Entity was null!");
+            throw new IllegalStateException("Entity was null");
         }
-        txtId.setText(String.valueOf(entity.getId()));
-        txtName.setText(entity.getName());
+        if (service == null) {
+            throw new IllegalStateException("Service was null");
+        }
+        try {
+            entity = getFormData();
+            service.saveOrUpdate(entity);
+            Utils.currentStage(event).close();
+        } catch (DbException e) {
+            Alerts.showAlert("Error saving object", null, e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+    private Department getFormData() {
+        Department obj = new Department();
+        obj.setId(Utils.tryParseToInt(txtId.getText()));
+        obj.setName(txtName.getText());
+
+        return obj;
     }
 
     @FXML
-    public void onBtSaveAction() {
-        System.out.println("Save button");
-    }
-
-    @FXML
-    public void onBtCancelAction() {
-        System.out.println("Cancel button");
+    public void onBtCancelAction(ActionEvent event) {
+        Utils.currentStage(event).close();
     }
 
 
@@ -57,5 +80,13 @@ public class DepartmentFormController implements Initializable {
     private void initializeNodes() {
         Constraints.setTextFieldInteger(txtId);
         Constraints.setTextFieldMaxLength(txtName, 30);
+    }
+
+    public void updateFormData() {
+        if (entity == null) {
+            throw new IllegalStateException("Entity was null!");
+        }
+        txtId.setText(String.valueOf(entity.getId()));
+        txtName.setText(entity.getName());
     }
 }
--- src/main/java/com/projetojavafx/javafxjdbcworkshop/gui/util/Utils.java
+++ src/main/java/com/projetojavafx/javafxjdbcworkshop/gui/util/Utils.java
@@ -1,11 +1,23 @@
 package com.projetojavafx.javafxjdbcworkshop.gui.util;
 
+import com.projetojavafx.javafxjdbcworkshop.model.dao.DaoFactory;
+import com.projetojavafx.javafxjdbcworkshop.model.dao.DepartmentDao;
+import com.projetojavafx.javafxjdbcworkshop.model.entities.Department;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.stage.Stage;
 
 public class Utils {
+
     public static Stage currentStage(ActionEvent event) {
         return (Stage) ((Node) event.getSource()).getScene().getWindow();
+    }
+
+    public static Integer tryParseToInt(String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            return null;
+        }
     }
 }
